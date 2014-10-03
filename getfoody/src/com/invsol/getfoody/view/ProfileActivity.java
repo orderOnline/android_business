30aac1aab9716547499d13d37d09477939f66746
rachnakhokhar <rachnakhokhar@gmail.com>
2014-10-04 00:37:35 +0530

    Added Styles For Action Bar.

---
+++ getfoody/src/com/invsol/getfoody/view/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.invsol.getfoody.view;
+
+import com.invsol.getfoody.R;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class ProfileActivity extends ActionBarActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_profile);
+	}
+}
