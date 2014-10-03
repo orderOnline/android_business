44fd03a5ea7df29e0258b0293f957e3ed928eb98
rachnakhokhar <rachnakhokhar@gmail.com>
2014-10-03 23:47:10 +0530

    Added Fragments For Home Activity.

--- getfoody/src/com/invsol/getfoody/fragments/RecentOrdersFragment.java
+++ getfoody/src/com/invsol/getfoody/fragments/RecentOrdersFragment.java
@@ -1,7 +1,21 @@
 package com.invsol.getfoody.fragments;
 
+import com.invsol.getfoody.R;
+
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class RecentOrdersFragment extends Fragment{
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view = inflater.inflate(R.layout.fragment_recentorders, container,
+				false);
+		return view;
+	}
 }
