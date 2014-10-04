ed9e991b15e39488d474de085f626345bbce3790
rachnakhokhar <rachnakhokhar@gmail.com>
2014-10-05 00:55:30 +0530

    Added Code For Profile And Menu Screen Designing.

--- getfoody/src/com/invsol/getfoody/fragments/ProfileReviewsFragment.java
+++ getfoody/src/com/invsol/getfoody/fragments/ProfileReviewsFragment.java
@@ -1,12 +1,15 @@
 package com.invsol.getfoody.fragments;
 
-import com.invsol.getfoody.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.invsol.getfoody.R;
+import com.invsol.getfoody.dataobjects.ReviewsItem;
+import com.invsol.getfoody.fragmentadapters.ReviewsFragmentAdapter;
 
 public class ProfileReviewsFragment extends Fragment{
 	
@@ -16,6 +19,19 @@ public class ProfileReviewsFragment extends Fragment{
 		// TODO Auto-generated method stub
 		View view = inflater.inflate(R.layout.fragment_profile_reviews, container,
 				false);
+		
+		ListView listView_reviews_items = (ListView) view
+				.findViewById(R.id.listview_reviews);
+
+		ReviewsItem[] reviewsDataItems = new ReviewsItem[10];
+		for (int i = 0; i < 10; i++) {
+			reviewsDataItems[i] = new ReviewsItem("Rachna Khokhar", "2 hours ago", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.");
+		}
+
+		ReviewsFragmentAdapter adapter = new ReviewsFragmentAdapter(
+				view.getContext(), R.layout.fragment_profile_reviews, reviewsDataItems);
+		listView_reviews_items.setAdapter(adapter);
+		
 		return view;
 	}
 
--- getfoody/src/com/invsol/getfoody/view/ProfileActivity.java
+++ getfoody/src/com/invsol/getfoody/view/ProfileActivity.java
@@ -1,16 +1,150 @@
 package com.invsol.getfoody.view;
 
 import com.invsol.getfoody.R;
+import com.invsol.getfoody.adapters.ProfilePagerAdapter;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar.Tab;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
-public class ProfileActivity extends ActionBarActivity{
+public class ProfileActivity extends ActionBarActivity implements
+		ActionBar.TabListener {
+
+	private ViewPager viewPager;
+	private ProfilePagerAdapter mAdapter;
+	private ActionBar actionBar;
+	private Menu profileMenu;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profile);
+
+		// Initilization
+		viewPager = (ViewPager) findViewById(R.id.profilepager);
+		actionBar = getSupportActionBar();
+		mAdapter = new ProfilePagerAdapter(getSupportFragmentManager());
+
+		viewPager.setAdapter(mAdapter);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setHomeButtonEnabled(true);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		Resources itemTexts = getResources();
+		String[] tabs = itemTexts.getStringArray(R.array.profile_tabs_array);
+
+		// Adding Tabs
+		for (String tab_name : tabs) {
+			Tab newTab = actionBar.newTab();
+			newTab.setText(tab_name);
+			newTab.setTabListener(this);
+			actionBar.addTab(newTab);
+		}
+
+		/**
+		 * on swiping the viewpager make respective tab selected
+		 * */
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int position) {
+				// on changing the page
+				// make respected tab selected
+				actionBar.setSelectedNavigationItem(position);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
 	}
+
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent event) {
+		View view = getCurrentFocus();
+		boolean ret = super.dispatchTouchEvent(event);
+
+		if (view instanceof EditText) {
+			View w = getCurrentFocus();
+			int scrcoords[] = new int[2];
+			w.getLocationOnScreen(scrcoords);
+			float x = event.getRawX() + w.getLeft() - scrcoords[0];
+			float y = event.getRawY() + w.getTop() - scrcoords[1];
+
+			if (event.getAction() == MotionEvent.ACTION_UP
+					&& (x < w.getLeft() || x >= w.getRight() || y < w.getTop() || y > w
+							.getBottom())) {
+				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(getWindow().getCurrentFocus()
+						.getWindowToken(), 0);
+			}
+		}
+		return ret;
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction arg1) {
+		// on tab selected
+		/*
+		 * // show respected fragment view MenuItem item =
+		 * profileMenu.findItem(R.id.action_profile); switch (tab.getPosition())
+		 * { case 0: { item.setIcon(R.drawable.ic_action_content_edit);
+		 * item.setTitle(R.string.text_profile_edit_menu); } break; case 1: {
+		 * item.setVisible(false); }
+		 * 
+		 * break; case 2: { item.setIcon(R.drawable.ic_action_content_save);
+		 * item.setTitle(R.string.text_profile_savepreferences_menu); }
+		 * 
+		 * break; default: break; }
+		 */
+		viewPager.setCurrentItem(tab.getPosition());
+
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * @Override public boolean onCreateOptionsMenu(Menu menu) { MenuInflater
+	 * inflater = getMenuInflater();
+	 * inflater.inflate(R.menu.activity_profile_menu, menu); this.profileMenu =
+	 * menu; return super.onCreateOptionsMenu(menu); }
+	 * 
+	 * Called whenever we call invalidateOptionsMenu()
+	 * 
+	 * @Override public boolean onPrepareOptionsMenu(Menu menu) {
+	 * this.profileMenu = menu; return super.onPrepareOptionsMenu(menu); }
+	 * 
+	 * @Override public boolean onOptionsItemSelected(MenuItem item) { // Handle
+	 * presses on the action bar items switch (item.getItemId()) { case
+	 * R.id.action_profile: { Intent screenChangeIntent = null;
+	 * screenChangeIntent = new Intent(ProfileActivity.this,
+	 * EditProfileActivity.class);
+	 * ProfileActivity.this.startActivity(screenChangeIntent); return true; }
+	 * default: return super.onOptionsItemSelected(item); } }
+	 */
 }
