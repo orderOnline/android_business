e80e2ee4303d41cf68257c1fc4eea5160b617e5e
rachnakhokhar <rachnakhokhar@gmail.com>
2014-10-19 13:00:33 +0530

    Removed unused styles.

--- getfoody/src/com/invsol/getfoody/view/CuisinesActivity.java
+++ getfoody/src/com/invsol/getfoody/view/CuisinesActivity.java
@@ -1,8 +1,12 @@
 package com.invsol.getfoody.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.invsol.getfoody.R;
 import com.invsol.getfoody.adapters.CuisinesAdapter;
@@ -26,6 +30,18 @@ public class CuisinesActivity extends ActionBarActivity{
 		CuisinesAdapter adapter = new CuisinesAdapter(
 				this, R.layout.activity_cuisines, cuisinesDataItems);
 		listView_reviews_items.setAdapter(adapter);
+		
+		TextView btn_next = (TextView)findViewById(R.id.btn_next);
+		btn_next.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				Intent screenChangeIntent = null;
+				screenChangeIntent = new Intent(CuisinesActivity.this,
+						MenuActivity.class);
+				CuisinesActivity.this.startActivity(screenChangeIntent);
+			}
+		});
 
 	}
 }
